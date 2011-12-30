6070ec13456e3aa128f77fc51269084d09c81d08
Jonty Sewell <jontysewell@gmail.com>
2011-12-30 13:45:52 +0000

    fixed the music index page

--- music/index.php
+++ music/index.php
@@ -4,7 +4,7 @@ Output::set_title("Music Library");
 Output::add_stylesheet(SITE_LINK_REL."css/music.css");
 
 MainTemplate::set_subtitle("Add and remove tracks, edit track details");
-$tracks = Search::tracks("cheese",100,0);
+$tracks = Tracks::get_newest();
 
 function track_length($time_arr) {
 	$time_str = ($time_arr["days"])? $time_arr["days"]."d " : "";
@@ -13,34 +13,32 @@ function track_length($time_arr) {
 	$time_str .= ($time_arr["seconds"])? $time_arr["seconds"]."s " : "0s ";
 	return $time_str;
 }
-
 if($tracks) {
+	echo("<h2>10 newest tracks</h2>");
 	echo("<table class=\"zebra-striped\" cellspacing=\"0\">
 	<thead>
 		<tr>
 			<th class=\"icon\"> </th>
 			<th class=\"artist\">Artist</th>
 			<th class=\"title\">Title</th>
-			<th class=\"album\">Album</th>
+			<th class=\"album\">Date Added</th>
 			<th class=\"length\">Length</th> 
 			<th class=\"icon\"></th>
 			".((Session::is_admin() || Session::is_group_user("music_admin"))? "<th class=\"icon\"></th>" : "")."
 		</tr>
 	</thead>");
-	foreach($tracks as $track_id) {
-		$track = Tracks::get($track_id);
+	foreach($tracks as $track) {
 		$artists = Artists::get_by_audio_id($track->get_id());
 		$artist_str = "";
 		foreach($artists as $artist) $artist_str .= $artist->get_name()."; ";
 		$artist_str = substr($artist_str,0,-2);
-		$album = Albums::get_by_audio_id($track->get_id());
-		$album = $album->get_name();
+		$import_date = date("d/m/Y H:i",$track->get_import_date());
 		echo("
 		<tr>
 			<td><a href=\"detail/".$track->get_id()."\" class=\"track-info\"><img src=\"".SITE_LINK_REL."images/icons/information.png\"></a></td>
 			<td>".$artist_str."</td>
 			<td>".$track->get_title()."</td>
-			<td>".$album."</td>
+			<td>".$import_date."</td>
 			<td>".track_length(Time::seconds_to_dhms($track->get_length()))."</td>
 			<td><a href=\"preview/".$track->get_id()."\" class=\"track-preview\"><img src=\"".SITE_LINK_REL."images/icons/sound.png\"></td>
 			".((Session::is_admin() || Session::is_group_user("music_admin"))? "<td><a href=\"delete/".$track->get_id()."\" class=\"track-delete\"><img src=\"".SITE_LINK_REL."images/icons/delete.png\"></td>" : "")."
