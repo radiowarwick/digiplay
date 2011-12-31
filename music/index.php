6dc71827f50c946167cc2aae15f21181fc04ce60
Jonty Sewell <jontysewell@gmail.com>
2011-12-31 11:58:23 +0000

    Scrapped the AJAX idea here, I think the popover is best with a click-through to the detail page. More coming later

--- music/index.php
+++ music/index.php
@@ -2,25 +2,15 @@
 require_once('pre.php');
 Output::set_title("Music Library");
 Output::add_stylesheet(SITE_LINK_REL."css/music.css");
+Output::add_script(SITE_LINK_REL."js/bootstrap-twipsy.js");
+Output::add_script(SITE_LINK_REL."js/bootstrap-popover.js");
 
 echo("<script>
 	$(function () {
-		$('.track-info').click(function(event) {
-			event.preventDefault();
-			var row = $(this).parent().parent();
-			if(row.next().hasClass('detail')) {
-				row.next().slideToggle();
-			} else {
-    			var id = row.attr('id');
-    			$.ajax({
-	         		type: 'GET',
-         			url: '".SITE_LINK_REL."music/detail/index.php?id='+id+'&ajax=1',
-         			dataType: 'html',
-	         		success: function(data) {
-	    	         	row.after('<tr class=\"detail\"><td colspan=\"7\">'+data+'</td></tr>');
-         			}
-    			});
-    		}
+		$('.track-info').popover({'html': true, 'title': function() { return($(this).parent().parent().find('.title').html()) }, 'content': function() { 
+			var text = 'test';
+			return(text);
+			}
 		});
 	});
 </script>");
@@ -57,13 +47,13 @@ if($tracks) {
 		$import_date = date("d/m/Y H:i",$track->get_import_date());
 		echo("
 		<tr id=\"".$track->get_id()."\">
-			<td><a href=\"".SITE_LINK_REL."music/detail/".$track->get_id()."\" class=\"track-info\"><img src=\"".SITE_LINK_REL."images/icons/information.png\"></a></td>
-			<td>".$artist_str."</td>
-			<td>".$track->get_title()."</td>
-			<td>".$import_date."</td>
-			<td>".track_length(Time::seconds_to_dhms($track->get_length()))."</td>
-			<td><a href=\"preview/".$track->get_id()."\" class=\"track-preview\"><img src=\"".SITE_LINK_REL."images/icons/sound.png\"></td>
-			".((Session::is_admin() || Session::is_group_user("music_admin"))? "<td><a href=\"delete/".$track->get_id()."\" class=\"track-delete\"><img src=\"".SITE_LINK_REL."images/icons/delete.png\"></td>" : "")."
+			<td class=\"icon\"><a href=\"".SITE_LINK_REL."music/detail/".$track->get_id()."\" class=\"track-info\"><img src=\"".SITE_LINK_REL."images/icons/information.png\"></a></td>
+			<td class=\"artist\">".$artist_str."</td>
+			<td class=\"title\">".$track->get_title()."</td>
+			<td class=\"album\">".$import_date."</td>
+			<td class=\"length\">".track_length(Time::seconds_to_dhms($track->get_length()))."</td>
+			<td class=\"icon\"><a href=\"preview/".$track->get_id()."\" class=\"track-preview\"><img src=\"".SITE_LINK_REL."images/icons/sound.png\"></td>
+			".((Session::is_admin() || Session::is_group_user("music_admin"))? "<td class=\"icon\"><a href=\"delete/".$track->get_id()."\" class=\"track-delete\"><img src=\"".SITE_LINK_REL."images/icons/delete.png\"></td>" : "")."
 		</tr>");
 	}
 	echo("</table>");
