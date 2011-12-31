7eefaa900a35f2b85ece1cb1334330ce2f59a06f
Jonty Sewell <jontysewell@gmail.com>
2011-12-31 23:28:08 +0000

    first iteration of detail page. doesn't work. happy new year.

---
+++ ajax/meta-update.php
@@ -0,0 +1,10 @@
+<?php
+require_once("pre.php");
+Output::set_template();
+
+if(true){
+	echo "success";
+}else{
+	echo "error";
+}
+?>
--- music/detail/index.php
+++ music/detail/index.php
@@ -11,32 +11,141 @@ if(!$track = Tracks::get($_GET["id"])) {
 	exit("<h2>Invalid track ID</h2><h3>If you got to this page via a link from somewhere else on the site, there may be a bug.  A bug you should bug the techies about!</h3>");
 }
 
-$artists = Artists::get_by_audio_id($track->get_id());
-foreach($artists as $artist) $artist_str .= $artist->get_name()."; ";
-$artist_str = substr($artist_str,0,-2);
+echo("
+	<script>
+		$(function () {
+			$('.click-clear').focus(function() {
+				if($(this).attr('data-value') != $(this).val()) {
+					$(this).attr('data-value', $(this).val());
+					$(this).val('');
+				}
+			});
+			$('.click-clear').blur(function() {
+				if($(this).val() == '') {
+					$(this).val($(this).attr('data-value'));
+				}
+			});
+			$('.track-meta-form').submit(function(event) {
+				event.preventDefault();
+				$(this).find('.help-inline').remove();
+				submit = $(this).find('input[type=\"submit\"]');
+				submit.button('loading');
+				$.post('".SITE_LINK_REL."ajax/meta-update', $(this).serialize(), function(data) {
+					if(data == \"success\") { 
+						submit.button('reset');
+					} else {
+						submit.after('<span class=\"help-inline\">'+data+'</span>');
 
-$album = Albums::get_by_audio_id($track->get_id());
-
-if(isset($_GET['ajax'])) {
-	Output::set_template();
-	echo("
+						submit.button('reset');
+					}
+				})
+			});
+		});
+	</script>
+	<h2>Track ID: ".$track->get_id()."</h2>
 	<div class=\"row\">
-		<div class=\"span16\">
-			<strong>".$track->get_title()." by ".$artist_str."</strong>
+		<div class=\"span7\">
+			<form class=\"track-detail-form\" action=\"\" method=\"post\">
+				<fieldset>
+					<div class=\"clearfix\">
+						<label for=\"title\">Title</label>
+						<div class=\"input\">
+							<input name=\"title\" class=\"required".$disabled."\" value=\"".$track->get_title()."\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"artist\">Artists</label>");
+						foreach($track->get_artist() as $artist) {
+							echo("
+						<div class=\"input\">
+							<input name=\"artist\" class=\"required".$disabled."\" value=\"".$artist->get_name()."\">
+						</div>");
+						}
+					echo("
+						<div class=\"input\">
+							<input name=\"new_artist[]\" class=\"click-clear".$disabled."\" value=\"Add new artist...\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"album\">Album</label>
+						<div class=\"input\">
+							<input name=\"album\" class=\"required".$disabled."\" value=\"".$track->get_album()->get_name()."\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"year\">Year</label>
+						<div class=\"input\">
+							<input name=\"year\" class=\"".$disabled."\" value=\"".$track->get_year()."\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"length\">Length</label>
+						<div class=\"input\">
+							<span class=\"uneditable-input\">".$track->get_length_formatted()."</span>
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"origin\">Origin</label>
+						<div class=\"input\">
+							<input name=\"origin\" class=\"required".$disabled."\" value=\"".$track->get_origin()."\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"reclibid\">Reclib ID</label>
+						<div class=\"input\">
+							<input name=\"reclibid\" class=\"".$disabled."\" value=\"".$track->get_reclibid()."\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"censored\">Censored</label>
+						<div class=\"input\">
+							<input type=\"checkbox\" name=\"censored\" class=\"".$disabled."\" ".($track->is_censored()? "checked" : "").">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"sustainer\">On Sue</label>
+						<div class=\"input\">
+							<input type=\"checkbox\" name=\"sustainer\" class=\"".$disabled."\" ".($track->is_sustainer()? "checked" : "").">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<div class=\"input\">
+							<input type=\"submit\" class=\"btn primary\" value=\"Save\">
+						</div>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+		<div class=\"span5\">
+			<form class=\"track-meta-form form-stacked\" action=\"".SITE_LINK_REL."ajax/meta-update\" method=\"POST\">
+				<fieldset>
+					<div class=\"clearfix\">
+						<label for=\"notes\">Notes</label>
+						<div class=\"input\">
+							<textarea name=\"notes\" class=\"".$disabled."\">".$track->get_notes()."</textarea>
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<label for=\"keyword\">Keywords</label>");
+						foreach($track->get_keywords() as $keyword) {
+							echo("
+						<div class=\"input\">
+							<input name=\"keyword\" class=\"".$disabled."\" value=\"".$keyword->get_text()."\">
+						</div>");
+						}
+					echo("
+						<div class=\"input\">
+							<input name=\"new_keyword[]\" class=\"click-clear".$disabled."\" value=\"Add new keyword...\">
+						</div>
+					</div>
+					<div class=\"clearfix\">
+						<div class=\"input\">
+							<input type=\"submit\" class=\"btn primary\" value=\"Save\">
+						</div>
+					</div>
+				</fieldset>
+			</form>
 		</div>
 	</div>
-	<div class=\"row\">
-		<div class=\"span8\">
-			<em>Album:</em> ".$album->get_name()."<br />
-			<em>Year:</em> ".($track->get_year()? $track->get_year() : "(none)")."<br />
-			<em>Length:</em> blah<br />
-		</div>
-		<div class=\"span8\">
-			<em>Some more info:</em> blah
-		</div>
-	</div>
-	");
-} else {
-	
-}
+");
 ?>
