847a13ec17fd3ac2d99bd531b175821e918bb4e7
Jonty Sewell <jontysewell@gmail.com>
2012-01-01 16:38:06 +0000

    updated audio/track classes a bit. probably many errors.

--- includes/Audio.php
+++ includes/Audio.php
@@ -4,8 +4,6 @@ class Audio {
 	protected $md5;
 	protected $archive;
 	protected $length_smpl;
-	protected $start_smpl;
-	protected $end_smpl;
 	protected $type;
 	protected $creator;
 	protected $creation_date;
@@ -16,32 +14,29 @@ class Audio {
 	protected $rip_result;
 	protected $filetype;
 
-	public function get_id() {
-		return $this->id;
-	}
+	public function get_id() { return $this->id; }
+	public function get_md5() { return $this->md5; }
+	public function get_archive() { return Archives::get($this->archive); }
+	public function get_length() { return $this->length_smpl / 44100; }
+	public function get_type() { return AudioTypes::get_by_id($this->type); }
+	public function get_creator() { return Users::get_by_id($this->creator); }
+	public function get_creation_date() { return $this->creation_date; }
+	public function get_import_date() { return $this->import_date; }
+	public function get_title() { return $this->title; }
+	public function get_origin() { return $this->origin; }
+	public function get_notes() { return $this->notes; }
+	public function get_rip_result() { return $this->rip_result; }
+	public function get_filetype() { return $this->filetype; }
 
-	public function get_title() {
-		return $this->title;
-	}
+	public function set_type($audiotype) { $this->type = $audiotype->get_id(); }
+	public function set_creator($user) { $this->creator = $user->get_id(); }
+	public function set_title($title) { $this->title = $title; }
+	public function set_origin($origin) { $this->origin = $origin; }
+	public function set_notes($notes) { $this->notes = $notes; }
 
-	public function get_length() {
-		return $this->length_smpl / 44100;
-	}
-
-	public function get_import_date() {
-		return $this->import_date;
-	}
-
-	public function get_origin() {
-		return $this->origin;
-	}
-
-	public function get_notes() {
-		return $this->notes;
-	}
-
-	public function get_filetype() {
-		return $this->filetype;
+	public function save() {
+		$sql = "UPDATE audio SET (type,creator,title,origin,notes) = (".pg_escape_string($this->type).",".pg_escape_string($this->creator).",'".pg_escape_string($this->title)."','".pg_escape_string($this->origin)."','".pg_escape_string($this->notes)."') WHERE id = ".$this->id.";";
+		return (DigiplayDB::query($sql))?;
 	}
 
 	/* Extended functions */
--- includes/Track.php
+++ includes/Track.php
@@ -8,45 +8,71 @@ class Track extends Audio{
 	protected $flagged;
 	protected $censor;
 
+	public function get_album() { return Albums::get_by_audio_id($this->id); }
+	public function get_track() { return $this->music_track; }
+	public function get_year() { return $this->music_released; }
+	public function get_reclibid() { return $this->reclibid; }
+	public function is_sustainer(){ return (($sustainer == "t")? TRUE : FALSE); }
+	public function is_flagged() { return (($flagged == "t")? TRUE : FALSE); }
+	public function is_censored(){ return (($censored == "t")? TRUE : FALSE); }
 
-	public function get_artist(){
-		return Artists::get_by_audio_id($this->id);
-	}
-
-	public function get_album(){
-		return Albums::get_by_audio_id($this->id);
-	}
-
-	public function get_keywords() {
-		return Keywords::get_by_audio_id($this->id);
-	}
-
-	public function get_year(){
-		return $this->music_released;
-	}
-
-	public function get_reclibid() {
-		return $this->reclibid;
-	}
-
-	public function is_sustainer(){
-		return (($sustainer == 't')? TRUE : FALSE);
-	}
-
-	public function is_censored(){
-		return (($censored == 't')? TRUE : FALSE);
-	}
-
-	public function is_flagged() {
-		return (($flagged == 't')? TRUE : FALSE);
-	}
+	public function set_album($album) { $this->music_album = $album->get_id(); }
+	public function set_track($track) { $this->track = $track; }
+	public function set_year($year) { $this->music_released = $year; }
+	public function set_reclibid($reclibid) { $this->reclibid = $reclibid; }
+	public function set_sustainer($sustainer) { $this->sustainer = $sustainer? "t":"f"; }
+	public function set_flagged($flagged) { $this->flagged = $flagged? "t":"f"; }
+	public function set_censor($censor) { $this->censor = $censor? "t":"f";}
 
 	/* Extended functions */
+	public function get_artists() { return Artists::get_by_audio_id($this->id); }
+	public function get_keywords() { return Keywords::get_by_audio_id($this->id); }
+	
 	public function get_artists_str() {
 		$artists = $this->get_artist();
 		foreach($artists as $artist) $artist_str .= $artist->get_name()."; ";
 		$artist_str = substr($artist_str,0,-2);
 		return $artist_str;
+	}
+
+	public function add_artists($artists) {
+		if(!is_array($artists)) {
+			$tmp = $artists;
+			$artists = array($tmp);
+		}
+		foreach($artists as $artist) {
+			$artist->add_to_track($this->id);
+		}
+	}
+
+	public function del_artists($artists) {
+		if(!is_array($artists)) {
+			$tmp = $artists;
+			$artists = array($tmp);
+		}
+		foreach($artists as $artist) {
+			$artist->del_from_track($this->id);
+		}
+	}
+		
+	public function add_keywords($keywords) {
+		if(!is_array($keywords)) {
+			$tmp = $keywords;
+			$keywords = array($tmp);
+		}
+		foreach($keywords as $keyword) {
+			$keyword->add_to_track($this->id);
+		}
+	}
+
+	public function del_keywords($keywords) {
+		if(!is_array($keywords)) {
+			$tmp = $keywords;
+			$keywords = array($tmp);
+		}
+		foreach($keywords as $keyword) {
+			$keyword->del_from_track($this->id);
+		}
 	}
 }
 
