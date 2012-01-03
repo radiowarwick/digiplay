4b30648712db90bce7527a82daa93001d5cd1a38
Jonty Sewell <jontysewell@gmail.com>
2012-01-03 11:49:06 +0000

    spoke too soon. fix for detecting censorship

--- includes/Track.php
+++ includes/Track.php
@@ -14,7 +14,7 @@ class Track extends Audio{
 	public function get_reclibid() { return $this->reclibid; }
 	public function is_sustainer(){ return (($this->sustainer == "t")? TRUE : FALSE); }
 	public function is_flagged() { return (($this->flagged == "t")? TRUE : FALSE); }
-	public function is_censored(){ return (($this->censored == "t")? TRUE : FALSE); }
+	public function is_censored(){ return (($this->censor == "t")? TRUE : FALSE); }
 
 	public function set_track($track) { $this->track = $track; }
 	public function set_year($year) { $this->music_released = (is_int($year))? $year : 0; }
