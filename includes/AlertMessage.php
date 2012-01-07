4734488638cc6f4736b78812accce8e819bec1c5
Jonty Sewell <jontysewell@gmail.com>
2012-01-07 22:31:48 +0000

    Added an AlertMessage class for bootstrap abstraction

---
+++ includes/AlertMessage.php
@@ -0,0 +1,21 @@
+<?php
+class AlertMessage {
+	public function basic($class="info",$text="",$title="",$close=true) {
+		$return = "<div class=\"alert-message ".$class.($close? " fade in" : "")."\">";
+		if($close) $return .= "<a class=\"close\" href=\"#\">&times;</a>";
+		if($title) $return .= "<strong>".$title."</strong> ";
+		$return .= $text;
+		$return .= "</div>";
+		return $return;
+	}	
+
+	public function block($class="info",$text="",$title="",$close=false) {
+		$return = "<div class=\"alert-message block-message ".$class.($close? " fade in" : "")."\">";
+		if($close) $return .= "<a class=\"close\" href=\"#\">&times;</a>";
+		if($title) $return .= "<strong>".$title."</strong> ";
+		$return .= $text;
+		$return .= "</div>";
+		return $return;
+	}
+}
+?>
