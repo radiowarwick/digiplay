bbf2050cb7d1a257ebc46191c651092de5d57db1
Jonty Sewell <jontysewell@gmail.com>
2012-02-10 19:58:51 +0000

    fixing alert messages for bootstrap-2.0

--- includes/AlertMessage.php
+++ includes/AlertMessage.php
@@ -1,20 +1,20 @@
 <?php
 class AlertMessage {
 	public function basic($class="info",$text="",$title="",$close=true) {
-		$return = "<div class=\"alert-message ".$class.($close? " fade in" : "")."\">";
+		$return = "<div class=\"alert ".$class.($close? " fade in" : "")."\">";
 		if($close) $return .= "<a class=\"close\" href=\"#\">&times;</a>";
-		if($title) $return .= "<p><strong>".$title."</strong> ";
+		if($title) $return .= "<strong>".$title."</strong> ";
 		$return .= $text;
-		$return .= "</p></div>";
+		$return .= "</div>";
 		return $return;
 	}	
 
 	public function block($class="info",$text="",$title="",$close=false) {
-		$return = "<div class=\"alert-message block-message ".$class.($close? " fade in" : "")."\"><p>";
+		$return = "<div class=\"alert-block ".$class.($close? " fade in" : "")."\">";
 		if($close) $return .= "<a class=\"close\" href=\"#\">&times;</a>";
 		if($title) $return .= "<strong>".$title."</strong> ";
 		$return .= $text;
-		$return .= "</p></div>";
+		$return .= "</div>";
 		return $return;
 	}
 }
