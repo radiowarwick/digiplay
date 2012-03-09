75d46dd4cb6864a9066de988d91c903a987b5b60
Jonty Sewell <jontysewell@gmail.com>
2012-03-09 15:18:41 +0000

    Added JSON search for main template navbar search box

---
+++ ajax/json-search.php
@@ -0,0 +1,91 @@
+<?php
+require_once("pre.php");
+Output::set_template();
+
+$query = $_REQUEST['q'];
+
+
+if($query) $search = Search::tracks($query,"title",5);
+$tracks = $search["results"];
+$tracks_array = array();
+
+if($tracks) {	
+	foreach($tracks as $track_id) {
+		$track_object = Tracks::get($track_id);
+		$track = array(
+			'id' => $track_object->get_id(),
+			'title' => "<b>".$track_object->get_title()."</b> by ".$track_object->get_artists_str(),
+			'href' => SITE_LINK_ABS."music/detail/".$track_object->get_id()
+			);
+		array_push($tracks_array, $track);
+	}
+	
+}
+
+if($query) $search = Search::tracks($query,"artist",5);
+$artists = $search["results"];
+$artists_array = array();
+
+if($artists) {	
+	foreach($artists as $artist_id) {
+		$track = Tracks::get($artist_id);
+		$artists = $track->get_artists();
+		foreach ($artists as $artist_object) {
+			$skip = false;
+			foreach($artists_array as $curr) {
+				if(($curr["title"] == $artist_object->get_name())) {
+					$skip = true;
+				}
+			}
+			if(!$skip) {
+				$artist = array(
+						'id' => $artist_object->get_id(),
+						'title' => $artist_object->get_name(),
+						'href' => SITE_LINK_ABS."music/search/?q=".$artist_object->get_name()."&i=artist"
+						);
+					array_push($artists_array, $artist);
+			}
+		}
+	}
+}
+
+
+if($query) $search = Search::tracks($query,"album",5);
+$albums = $search["results"];
+$albums_array = array();
+
+if($albums) {	
+	foreach($albums as $album_id) {
+		$track = Tracks::get($album_id);
+		$album_object = $track->get_album();
+		$skip = false;
+		foreach($albums_array as $curr) {
+			if(($curr["title"] == $album_object->get_name())) {
+				$skip = true;
+			}
+		}
+		if(!$skip) {
+			$album = array(
+				'id' => $album_object->get_id(),
+				'title' => $album_object->get_name(),
+				'href' => SITE_LINK_ABS."music/search/?q=".$artist_object->get_name()."&i=album"
+				);
+			array_push($albums_array, $album);
+		}
+	}
+}
+
+$array = array(
+	array( 
+		"title" => "Tracks",
+		"data" => $tracks_array),
+	array(
+		"title" => "Artists",
+		"data" => $artists_array),
+	array(
+		"title" => "Albums",
+		"data" => $albums_array)
+	);
+
+echo json_encode($array);
+?>
