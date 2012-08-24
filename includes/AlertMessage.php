6f8716bbe43767aeeae13a7e37e47409feab34b7
Jonty Sewell <jontysewell@gmail.com>
2012-08-24 11:57:16 +0100

    Fixed AlertMessage class for newest boostrap alert classes

--- includes/AlertMessage.php
+++ includes/AlertMessage.php
@@ -1,7 +1,7 @@
 <?php
 class AlertMessage {
 	public function basic($class="info",$text="",$title="",$close=true) {
-		$return = "<div class=\"alert ".$class.($close? " fade in" : "")."\">";
+		$return = "<div class=\"alert alert-".$class.($close? " fade in" : "")."\">";
 		if($close) $return .= "<a class=\"close\" href=\"#\">&times;</a>";
 		if($title) $return .= "<strong>".$title."</strong> ";
 		$return .= $text;
@@ -10,7 +10,7 @@ class AlertMessage {
 	}	
 
 	public function block($class="info",$text="",$title="",$close=false) {
-		$return = "<div class=\"alert-block ".$class.($close? " fade in" : "")."\">";
+		$return = "<div class=\"alert-block alert-".$class.($close? " fade in" : "")."\">";
 		if($close) $return .= "<a class=\"close\" href=\"#\">&times;</a>";
 		if($title) $return .= "<strong>".$title."</strong> ";
 		$return .= $text;
