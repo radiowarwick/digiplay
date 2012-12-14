12f1fd5d7366a64c73d9394512182db36da64354
Jonty Sewell <jontysewell@gmail.com>
2012-12-14 15:27:51 +0000

    New server = much faster searches = sphinx is more trouble than it's worth. Fun learning exercise though

--- includes/Search.php
+++ includes/Search.php
@@ -1,35 +1,40 @@
 <?php
 class Search {
-    public function tracks($query,$fields="title,artist,album,keyword",$limit=0,$offset=0,$group=NULL) {
-        $cl = new SphinxClient();
-        $cl->SetMatchMode(SPH_MATCH_EXTENDED);
-        $cl->SetSortMode(SPH_SORT_RELEVANCE);
-        if($limit) $cl->SetLimits($offset,$limit);
-        if($group) $cl->SetGroupBy("attr_".$group, SPH_GROUPBY_ATTR);
-
+    public function tracks($query,$fields="title,artist,album",$limit=0,$offset=0) {
+        // make sure the caller hasn't used spaces
         $fields = str_replace(" ",",",$fields);
-        $query_str = "@@relaxed @(".$fields.") ".$query;
 
-        $result = $cl->Query($query_str);
-        if ($result === false) throw new UserError("Query failed: " . $cl->GetLastError());
-        else if ($cl->GetLastWarning()) throw new UserError("WARNING: " . $cl->GetLastWarning());
+        // form a query string for v_audio_music
+        $query_str = "SELECT id, count(*) OVER() AS full_count FROM v_audio_music WHERE dir = 2";
+
+        // modge the field specifiers into tsquery syntax
+        $fields_query = str_replace(","," || ' ' || ", $fields);
+        
+        // add the bit that actually searches
+        $query_str .= " AND to_tsvector(".$fields_query.")::tsvector @@ plainto_tsquery('".$query."')::tsquery ORDER BY id DESC";
+
+        // don't forget to limit/offset for pagination
+        if($limit > 0) $query_str .= " LIMIT ".$limit;
+        if($offset > 0) $query_str .= " OFFSET ".$offset;
+
+        // QUERY MOFOS
+        $result = DigiplayDB::query($query_str);
+        if ($result === false) throw new UserError("Query failed");
 
         $results = array();
-        if (!empty($result["matches"])) {
-            foreach ($result["matches"] as $id => $info) {
-                $results[]= $id;
+        if(pg_num_rows($result) > 0) {
+            while($item = pg_fetch_assoc($result,NULL)) {
+                $results[] = $item["id"];
+                $total = $item["full_count"];
             }
+            
         }
+
         $return = array(
             "results" => $results,
-            "total" => $result["total"],
-            "time" => $result["time"]);
-        return ((count($results) > 0)? $return : NULL);
-    }
+            "total" => $total);
 
-    public function update_index() {
-        $return = shell_exec("/usr/bin/indexer --quiet --rotate --all");
-        return ($return)? false:true;
+        return ((count($results) > 0)? $return : NULL);
     }
 }
 
