72cfa82e409efd175e9c866c01d4e56bd5e03bca
jonty-comp <jontysewell@gmail.com>
2013-08-20 11:52:08 +0100

    Update to Bootstrap 3.0.0 from bootstrapCDN

--- includes/MainTemplate.php
+++ includes/MainTemplate.php
@@ -57,16 +57,9 @@ class MainTemplate implements Template{
 		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
 		<script type=\"text/javascript\" src=\"//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js\"></script>
+		<script type=\"text/javascript\" src=\"//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js\"></script>
+		<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css\">
 		";
-		if(Session::is_developer()) {
-			$return .= "<script type=\"text/javascript\" src=\"".LINK_ABS."js/bootstrap.js\"></script>
-		<link rel=\"stylesheet\" href=\"".LINK_ABS."css/bootstrap.css\">
-		";
-		} else {
-			$return .= "<script type=\"text/javascript\" src=\"".LINK_ABS."js/bootstrap.min.js\"></script>
-		<link rel=\"stylesheet\" href=\"".LINK_ABS."css/bootstrap.min.css\">
-		";
-		}
 	if(count(Output::get_less_stylesheets())>0) {
 		foreach(Output::get_less_stylesheets() AS $src){
 			$return .= "<link href=\"".$src."\" rel=\"stylesheet/less\">
