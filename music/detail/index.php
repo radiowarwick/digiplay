732dcc9cf25708c95af51e74910f2ab8c8906d9e
jonty-comp <jontysewell@gmail.com>
2013-08-22 16:36:47 +0100

    massive tidy-up

--- music/detail/index.php
+++ music/detail/index.php
@@ -1,18 +1,10 @@
 <?php
 
 Output::set_title("Track Detail");
-Output::add_script(LINK_ABS."js/jquery.jplayer.min.js");
-Output::add_stylesheet(LINK_ABS."css/jplayer.css");
-
 MainTemplate::set_subtitle("View and edit track metadata");
 
-if(!isset($_GET['id'])) {
-	exit("<h3>No track specified</h3><h4>You must access this via another page, to get metadata for a specified track.</h4>");
-}
-
-if(!$track = Tracks::get($_GET["id"])) {
-	exit("<h3>Invalid track ID</h3><h4>If you got to this page via a link from somewhere else on the site, there may be a bug.  A bug you should bug the techies about!</h4>");
-}
+if(!isset($_GET['id'])) exit("<h3>No track specified</h3><h4>You must access this via another page, to get metadata for a specified track.</h4>");
+if(!$track = Tracks::get($_GET["id"])) exit("<h3>Invalid track ID</h3><h4>If you got to this page via a link from somewhere else on the site, there may be a bug.  A bug you should bug the techies about!</h4>");
 if(!Session::is_group_user("Music Admin")) $disabled = " disabled";
 
 echo("
@@ -20,37 +12,34 @@ echo("
 		$(function () {
 			$('.track-detail-form').submit(function(event) {
 				event.preventDefault();
-				submit = $(this).find('input[type=\"submit\"]');
-				submit.button('loading');
-				submit.after('<img src=\"".LINK_ABS."img/ajax-loader.gif\" id=\"detail-load\" style=\"position: relative; top: 5px; left: 5px;\"/>');
+				submit = $(this).find('button[type=\"submit\"]');
+				submit.find('span').removeClass('glyphicon-save').addClass('glyphicon-refresh');
 				$.ajax({
 					url: '".LINK_ABS."ajax/track-detail-update.php',
 					data: $(this).serialize(),
 					type: 'POST',
 					error: function(xhr,text,error) {
 						value = $.parseJSON(xhr.responseText);
-						$('#detail-load').remove();
-						$('h3').after('".Bootstrap::alert_message_basic("error","'+value.error+'","Error!")."');
+						submit.find('span').removeClass('glyphicon-refresh').addClass('glyphicon-save');
+						$('h3').after('".Bootstrap::alert_message_basic("danger","'+value.error+'","Error!")."');
 						$('.alert-message').alert();
-						submit.button('reset');
 					},
 					success: function(data,text,xhr) {
 						values = $.parseJSON(data);
-						submit.button('reset');
-						$('#detail-load').remove();
+						submit.find('span').removeClass('glyphicon-refresh').addClass('glyphicon-save');
 						$('h3').after('".Bootstrap::alert_message_basic("success","Track details altered.","Success!",false)."');
-						$('[name=new_artist]').val('');
-						$('[name=artist\\\\[\\\\]]').remove();
+						$('[id=new_artist]').val('');
+						$('[id=artist\\\\[\\\\]]').remove();
 						artists_str = '';
 						$.each(values.artists, function(i, val) {
-							artists_str += '<div class=\"controls\"><input type=\"text\" name=\"artist[]\" class=\"required".$disabled."\" value=\"'+val+'\"></div>';
+							artists_str += '<div class=\"col-md-10\"><input type=\"text\" id=\"artist[]\" name=\"artist[]\" class=\"form-control".$disabled."\" value=\"'+val+'\"></div>';
 						});
 						$('[for=artist]').after(artists_str);
-						$('[name=new_keyword]').val('');
+						$('[id=new_keyword]').val('');
 						$('.keyword').parent().remove();
 						keywords_str = '';
 						$.each(values.keywords, function(i, val) {
-							keywords_str += '<div class=\"input-group\"><span class=\"input-group-addon\"><a class=\"keyword-remove\" href=\"".LINK_ABS."ajax/del-keywords.php?track_id=".$track->get_id()."&keyword='+val+'\">".Bootstrap::glyphicon("remove-sign")."</a></span><input type=\"text\" class=\"input-medium\" disabled value=\"'+val+'\"></div></div>'
+							keywords_str += '<div class=\"input-group\"><span class=\"input-group-addon\"><a class=\"keyword-remove\" href=\"".LINK_ABS."ajax/del-keywords.php?track_id=".$track->get_id()."&keyword='+val+'\">".Bootstrap::glyphicon("remove-sign")."</a></span><input type=\"text\" class=\"form-control\" disabled value=\"'+val+'\"></div></div>'
 						});
 						$('.for-keywords').html(keywords_str);
 						setTimeout(function() {
@@ -68,30 +57,30 @@ echo("
 					if(data == \"success\") {
 						parent.remove();
 					} else {
-						$('h3').after('".Bootstrap::alert_message_basic("error","'+data+'","Error!")."');
+						$('h3').after('".Bootstrap::alert_message_basic("danger","'+data+'","Error!")."');
 						$('.alert-message').show('fast').alert();
 					}
 				});
 				return false;
 			});
 
-			$('[name=flag]').click(function() {
+			$('[id=flag]').click(function() {
 				event.preventDefault();
 				t = $(this);
-				t.after('<img src=\"".LINK_ABS."img/ajax-loader.gif\" id=\"flag-load\" style=\"position: relative; top: 5px; left: 5px;\"/>');
+				t.find('.censor-flag').removeClass('glyphicon-warning-sign').addClass('glyphicon-refresh');
 				$.ajax({
 					url: '".LINK_ABS."ajax/flag.php',
 					data: 'id=".$track->get_id()."&flag=toggle',
 					type: 'GET',
 					error: function(xhr,text,error) {
 						value = $.parseJSON(xhr.responseText);
-						$('h3').after('".Bootstrap::alert_message_basic("error","'+value.error+'","Error!")."');
+						$('h3').after('".Bootstrap::alert_message_basic("danger","'+value.error+'","Error!")."');
 						$('.alert-message').alert();
-						$('#flag-load').remove();
+						t.find('.censor-flag').removeClass('glyphicon-refresh').addClass('glyphicon-warning-sign');
 					},
 					success: function(data,text,xhr) {
 							value = $.parseJSON(xhr.responseText);
-							$('#flag-load').remove();
+							t.find('.censor-flag').removeClass('glyphicon-refresh').addClass('glyphicon-warning-sign');
 							if(value.response == 'flagged') {
 								t.addClass('active');
 								response = 'This track has been flagged for censorship and will be reviewed in due course.';
@@ -107,160 +96,116 @@ echo("
 					}
 				});
 			});
-
-   			$(\"#jquery_jplayer_1\").jPlayer({
-				ready: function(event) {
-					$(this).jPlayer(\"setMedia\", {
-						mp3: \"".LINK_ABS."lib/preview/preview.php?id=".$track->get_id()."\"
-					});
-				},
-				supplied: \"mp3\"
-			});
-			$(\"#jquery_jplayer_1\").bind($.jPlayer.event.loadeddata, function(event) {
-				$('.jp-controls li').show();
-				$('.jp-controls li.dps-file-loading').hide();
-				$('.jp-timestamp-placeholder').hide();
-				$('.jp-timestamp').show();
-			});
-			
-			$('.dps-waveform img').load(function(){
-				$('.dps-waveform-loading').fadeOut('fast', function(){ $('.dps-waveform').fadeIn(); });
-			});
-
 		});
 	</script>
 	<h3>Edit Track: ".$track->get_id()." <small>Added ".date("d/m/Y H:i",$track->get_import_date())."</small></h3>
 	".(Session::is_group_user("Music Admin")? "":Bootstrap::alert_message_basic("info","You can't edit the details of this track, because you aren't a Music Admin.","Notice:")));
 	echo("
-
-       <div id=\"jquery_jplayer_1\" class=\"jp-jplayer col-lg-12\"></div>
-        
-        <div id=\"jp_container_1\" class=\"jp-audio\">
-            <div class=\"jp-type-single\">
-                <div class=\"jp-gui jp-interface\">
-                    <ul class=\"jp-controls\">
-                        <li class=\"dps-file-loading\"><img src=\"".LINK_ABS."img/ajax-loader.gif\"></li>
-                        <li><a href=\"javascript:;\" class=\"jp-play\" tabindex=\"1\">play</a></li>
-                        <li><a href=\"javascript:;\" class=\"jp-pause\" tabindex=\"1\">pause</a></li>
-                    </ul>
-                    
-                    <div class=\"jp-progress-wrap\">
-                    <div class=\"jp-progress\">
-			   <img class=\"dps-waveform-loading\" src=\"".LINK_ABS."img/ajax-loader.gif\" />
-			   <div class=\"dps-waveform\"><img src=\"".LINK_ABS."lib/waveform/index.php?id=".$track->get_id()."\" /></div>
-                        <div class=\"jp-seek-bar\">
-                            <div class=\"jp-play-bar\"></div>
-                        </div>
-                    </div>
-			</div>
-			<div class=\"jp-timestamp-placeholder\">
-			Loading Audio
-			</div>
-			<div class=\"jp-timestamp\">
-                    <div class=\"jp-current-time\"></div> /
-                    <div class=\"jp-duration\"></div>    </div>               
-                </div>
-
-            </div>
-        </div>
 	<form class=\"track-detail-form\" action=\"\" method=\"post\">
 		<fieldset>
 			<div class=\"row\">
-				<div class=\"col-lg-7 form-horizontal\">
-					<input type=\"hidden\" name=\"id\" value=\"".$track->get_id()."\">
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"title\">Title</label>
-						<div class=\"controls\">
-							<input type=\"text\" name=\"title\" class=\"required\" ".$disabled." value=\"".$track->get_title()."\">
+				<div class=\"col-md-7 form-horizontal\">
+					<input type=\"hidden\" id=\"id\" name=\"id\" value=\"".$track->get_id()."\">
+					<div class=\"form-group\">
+						<label class=\"control-label col-md-2\" for=\"title\">Title</label>
+						<div class=\"col-md-10\">
+							<input type=\"text\" id=\"title\" name=\"title\" class=\"form-control\" ".$disabled." value=\"".$track->get_title()."\">
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"artist\">Artists</label>");
+					<div class=\"form-group\">
+						<label class=\"control-label col-md-2\" for=\"artist\">Artists</label>");
+						$first = true;
 						foreach($track->get_artists() as $key=>$artist) {
 							echo("
-						<div class=\"controls\">
-							<input type=\"text\" name=\"artist[]\" class=\"required\" ".$disabled." value=\"".$artist->get_name()."\">
+						<div class=\"col-md-10".(!$first? " col-md-offset-2" : "")."\">
+							<input type=\"text\" id=\"artist[]\"  name=\"artist[]\" class=\"form-control\" ".$disabled." value=\"".$artist->get_name()."\">
 						</div>");
+							$first = false;
 						}
 					echo("
-						<div class=\"controls\">
-							<input type=\"text\" name=\"new_artist\" class=\"click-clear\" ".$disabled." placeholder=\"Add new artist...\">
+						<div class=\"col-md-10 col-md-offset-2\">
+							<input type=\"text\" id=\"new_artist\" name=\"new-artist\" class=\"form-control\" ".$disabled." placeholder=\"Add new artist...\">
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"album\">Album</label>
-						<div class=\"controls\">
-							<input type=\"text\" name=\"album\" class=\"required\" ".$disabled." value=\"".$track->get_album()->get_name()."\">
+					<div class=\"form-group\">
+						<label class=\"control-label col-md-2\" for=\"album\">Album</label>
+						<div class=\"col-md-10\">
+							<input type=\"text\" id=\"album\" name=\"album\" class=\"form-control\" ".$disabled." value=\"".$track->get_album()->get_name()."\">
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"year\">Year</label>
-						<div class=\"controls\">
-							<input type=\"text\" name=\"year\" ".$disabled." value=\"".$track->get_year()."\">
+					<div class=\"form-group\">
+						<label class=\"control-label col-md-2\" for=\"year\">Year</label>
+						<div class=\"col-md-10\">
+							<input type=\"text\" class=\"form-control\" id=\"year\" name=\"year\" ".$disabled." value=\"".$track->get_year()."\">
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"length\">Length</label>
-						<div class=\"controls\">
-							<span class=\"uneditable-input\">".Time::format_succinct($track->get_length())."</span>
+					<div class=\"form-group\">
+						<label class=\"control-label col-md-2\" for=\"length\">Length</label>
+						<div class=\"col-md-10\">
+							<p class=\"form-control-static\">".Time::format_succinct($track->get_length())."</span>
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"origin\">Origin</label>
-						<div class=\"controls\">
-							<input type=\"text\" name=\"origin\" class=\"required\" ".$disabled." value=\"".$track->get_origin()."\">
+					<div class=\"form-group\">
+						<label class=\"control-label col-md-2\" for=\"origin\">Origin</label>
+						<div class=\"col-md-10\">
+							<input type=\"text\" id=\"origin\" name=\"origin\" class=\"form-control\" ".$disabled." value=\"".$track->get_origin()."\">
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<div class=\"checkbox\">
-							<label class=\"control-label\" for=\"censored\">
-								<input type=\"checkbox\" name=\"censored\" ".$disabled." ".($track->is_censored()? "checked" : "").">
-								Censored
-							</label>
+					<div class=\"form-group\">
+						<div class=\"col-md-10 col-md-offset-2\">
+							<div class=\"checkbox\">
+								<label for=\"censored\">
+									<input type=\"checkbox\" id=\"censored\" name=\"censored\" ".$disabled." ".($track->is_censored()? "checked" : "").">
+									Censored
+								</label>
+							</div>
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<div class=\"checkbox\">
-							<label class=\"control-label\" for=\"sustainer\">
-								<input type=\"checkbox\" name=\"sustainer\" ".$disabled." ".($track->is_sustainer()? "checked" : "").">
-								On Sustainer
-							</label>
+					<div class=\"form-group\">
+						<div class=\"col-md-10 col-md-offset-2\">
+							<div class=\"checkbox\">
+								<label for=\"sustainer\">
+									<input type=\"checkbox\" id=\"sustainer\" name=\"sustainer\" ".$disabled." ".($track->is_sustainer()? "checked" : "").">
+									On Sustainer
+								</label>
+							</div>
 						</div>
 					</div>
-					<div class=\"control-group\">
-						<div class=\"controls\">
-							<input type=\"submit\" class=\"btn btn-primary\" value=\"Save\">
+					<div class=\"form-group\">
+						<div class=\"col-md-10 col-md-offset-2\">
+							<button type=\"submit\" class=\"btn btn-primary btn-block\">
+								".Bootstrap::glyphicon("save save")."
+								Save
+							</button>
 						</div>
 					</div>
 				</div>
-				<div class=\"col-lg-5 form-stacked\">
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"notes\">Notes</label>
-						<div class=\"controls\">
-							<textarea name=\"notes\" ".$disabled.">".$track->get_notes()."</textarea>
-						</div>
+				<div class=\"col-md-5 form\">
+					<div class=\"form-group\">
+						<label for=\"notes\">Notes</label>
+						<textarea class=\"form-control\" id=\"notes\" name=\"notes\" ".$disabled.">".$track->get_notes()."</textarea>
 					</div>
-					<div class=\"control-group\">
-						<label class=\"control-label\" for=\"keyword\">Keywords</label>
+					<div class=\"form-group\">
+						<label for=\"keyword\">Keywords</label>
 						<div class=\"for-keywords\">");
 							foreach($track->get_keywords() as $keyword) {
 								echo("
 								<div class=\"input-group\">
 									<span class=\"input-group-addon\"><a class=\"keyword-remove\" href=\"".LINK_ABS."ajax/del-keywords.php?track_id=".$track->get_id()."&keyword=".$keyword->get_text()."\">".Bootstrap::glyphicon("remove-sign")."</a></span>
-									<input type=\"text\" class=\"input-medium\" disabled value=\"".$keyword->get_text()."\">
+									<input type=\"text\" class=\"form-control\" disabled value=\"".$keyword->get_text()."\">
 								</div>
 							");
 							}
 					echo("
 						</div>
-						<div class=\"input-prepend\">
-							<span class=\"add-on\">".Bootstrap::glyphicon("tag")."</span>
-							<input type=\"text\" name=\"new_keyword\" class=\"input-medium click-clear\" ".$disabled." placeholder=\"Add new keyword...\">
+						<div class=\"input-group\">
+							<span class=\"input-group-addon\">".Bootstrap::glyphicon("tag")."</span>
+							<input type=\"text\" id=\"new_keyword\" name=\"new_keyword\" class=\"form-control\" ".$disabled." placeholder=\"Add new keyword...\">
 						</div>
 					</div>
 					<hr />					
-					<a href=\"#\" name=\"flag\" class=\"btn btn-danger btn-block".($track->is_flagged()? " active" : "")."\">".Bootstrap::glyphicon("warning-sign icon-white")." Flag for censorship</a>
-					".(Session::is_group_user("Music Admin")? "<a href=\"".LINK_ABS."lib/preview/download.php?id=".$track->get_id()."\" class=\"btn btn-primary btn-block\">".Bootstrap::glyphicon("download-alt icon-white")." Download FLAC</a>" : "")."
+					<a href=\"#\" id=\"flag\" class=\"btn btn-danger btn-block".($track->is_flagged()? " active" : "")."\">".Bootstrap::glyphicon("warning-sign censor-flag")." Flag for censorship</a>
+					".(Session::is_group_user("Music Admin")? "<a href=\"".LINK_ABS."lib/preview/download.php?id=".$track->get_id()."\" class=\"btn btn-primary btn-block\">".Bootstrap::glyphicon("download-alt")." Download FLAC</a>" : "")."
 				</div>
 			</div>
 		</fieldset>
