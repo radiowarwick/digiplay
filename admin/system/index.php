0e3fef9ea4245d1d9eb9ef87fe7a52b5d78fa5f8
James Van Hinsbergh <jvanhinsbergh@raw.sunion.warwick.ac.uk>
2013-10-08 16:59:14 +0100

    Added modal dialogs for system settings!

--- admin/system/index.php
+++ admin/system/index.php
@@ -22,7 +22,33 @@ foreach($settings as $setting){
 					<strong>".$setting->get_val()."</strong> <br />
 				</td>
 				<td class=\"icon\">
-					<span class='glyphicon glyphicon-edit'></span>
+					
+					  <!-- Button trigger modal -->
+					  <a data-toggle=\"modal\" href=\"#".$setting->get_parameter()."\"><span class='glyphicon glyphicon-edit'></span></a>
+
+					  <!-- Modal -->
+					  <div class=\"modal fade\" id=\"".$setting->get_parameter()."\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
+					    <div class=\"modal-dialog\">
+					      <div class=\"modal-content\">
+					        <div class=\"modal-header\">
+					          <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
+					          <h4 class=\"modal-title\">Change Value for ".$setting->get_parameter()."</h4>
+					        </div>
+					        <div class=\"modal-body\">
+					          <form role=\"form\">
+								  <div class=\"form-group\">
+								    <label for=\"settingUpdateText\">Value for ".$setting->get_parameter()."</label>
+								    <input type=\"text\" class=\"form-control\" id=\"settingUpdateText\" placeholder=\"Enter new value\" value=\"".$setting->get_val()."\">
+								  </div>
+					        </div>
+					        <div class=\"modal-footer\">
+					          <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
+					          <button type=\"button\" class=\"btn btn-primary\">Save changes</button>
+					        </div>
+					        </form>
+					      </div><!-- /.modal-content -->
+					    </div><!-- /.modal-dialog -->
+					  </div><!-- /.modal -->
 				</td>
 			</tr>");
 }
