4d8518d2add8f02b62aeaf9f556dfdcc9c3535a4
jonty-comp <jontysewell@gmail.com>
2013-10-11 13:07:10 +0100

    Censor tracks within the defined censor hours

--- studio/manage/functions.php
+++ studio/manage/functions.php
@@ -47,6 +47,9 @@ switch($_REQUEST["action"]) {
 				<tbody>";
 			foreach($search["results"] as $track) {
 				$track = Tracks::get($track);
+				if((Configs::get_system_param("censor_start") < date("H")) && (date("H") < Configs::get_system_param("censor_end"))) $censor_time = true;
+				else $censor_time = false;
+				if($censor_time && $track->is_censored()) continue;
 				$return .= "<tr data-track-id=\"".$track->get_id()."\" class=\"track\">
 					<td class=\"icon\">".Bootstrap::glyphicon("music")."</td>
 					<td class=\"artist nowrap\">".$track->get_artists_str()."</td>
