91457f74fda2ecb4f5e16fa8c231277a3827f0df
jonty-comp <jontysewell@gmail.com>
2014-01-03 19:59:29 +0000

    Add a rudimentary file manager

---
+++ ajax/file-tree.php
@@ -0,0 +1,26 @@
+<?php
+
+if(!isset($_REQUEST["id"]) || !($file = Files::get_by_id($_REQUEST["id"]))) {
+	http_response_code(400);
+	exit(json_encode(array("error" => "Invalid directory specified, or no permission to read it.")));
+}
+
+$children = $file->get_children();
+
+$return = array();
+
+foreach($children as $child) {
+	$array = array(
+		"title" => $child->get_name(),
+		"key" => $child->get_itemtype().$child->get_id(),
+		"id" => $child->get_id(),
+		"type" => $child->get_itemtype()
+	);
+
+	if($child->get_itemtype() == "dir") $array["folder"] = true; 
+	if($child->has_children()) $array["lazy"] = true;
+
+	$return[] = $array;
+}
+
+echo(json_encode($return));
