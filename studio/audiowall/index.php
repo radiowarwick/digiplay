6689519b736cee9e20b5cfa64c14f998bc657455
jonty-comp <jontysewell@gmail.com>
2014-04-25 13:30:23 +0100

    Tweak audiowalls a bit

--- studio/audiowall/index.php
+++ studio/audiowall/index.php
@@ -85,20 +85,70 @@ echo("
 		</script>
 ");
 
-$set = AudiowallSets::get_by_id($location->get_config("station_aw_set")->get_val());
+if(isset($_GET["id"])) $set = AudiowallSets::get_by_id($_GET["id"]);
+else $set = AudiowallSets::get_by_id($location->get_config("station_aw_set")->get_val());
 
-$wall = $set->get_walls()[0];
+if(isset($_GET["page"])) $wall = $set->get_walls()[$_GET["page"]-1];
+else $wall = $set->get_walls()[0];
+
 $items = $wall->get_items();
 
 echo("<div class=\"row\">");
 for($i = 0; $i < 12; $i++) {
-	echo("<div class=\"col-xs-4\">");
+	echo("
+		<div class=\"col-xs-4\">");
 	if(isset($items[$i])) {
-		$style = $items[$i]->get_style();
-		$audio = $items[$i]->get_audio();
-		echo("<div class=\"btn btn-block\" style=\"color: ".$style->get_foreground_rgb()."; background: ".$style->get_background_rgb()."; border-color: ".$style->get_accent_rgb()."\">".$items[$i]->get_text()."<br />".$audio->get_length_formatted()."</div>");
+		$item = $items[$i];
+		$style = $item->get_style();
+		$audio = $item->get_audio();
+		echo("
+			<div class=\"btn btn-block disabled\" data-item-id=\"".$item->get_id()."\" style=\"color: ".$style->get_foreground_rgb()."; background: ".$style->get_background_rgb()."; border-color: ".$style->get_accent_rgb()."\">
+				<div class=\"buffer\"></div>
+				<div class=\"progress\"></div>
+				".$item->get_text()."<br />".$audio->get_length_formatted()."
+				<script>
+				$(function() {
+					$('[data-item-id=".$item->get_id()."]').find('.disabled').show();
+					item".$item->get_id()." = new Audio('".LINK_ABS."audio/get/".$audio->get_id().".wav');
+					item".$item->get_id().".buffercheck = setInterval(function() { 
+						if(item".$item->get_id().".buffered.length == 1) {
+							buffered = (item".$item->get_id().".buffered.end(0) / item".$item->get_id().".duration).toFixed(3) * 100;
+						 	$('[data-item-id=".$item->get_id()."]').find('.buffer').css('width', (100-buffered)+'%');
+
+						 	if(buffered == 100) { clearInterval(item".$item->get_id().".buffercheck) }
+						} 
+					}, 50);
+
+					$('[data-item-id=".$item->get_id()."]').bind('click', function() {
+						if($('[data-item-id=".$item->get_id()."]').hasClass('disabled')) return false;
+						if(item".$item->get_id().".paused == true || item".$item->get_id().".currentTime == 0) {
+							item".$item->get_id().".play();
+							item".$item->get_id().".progressupdate = setInterval(function() { 
+								progress = (item".$item->get_id().".currentTime / item".$item->get_id().".duration).toFixed(3) * 100;
+					 			$('[data-item-id=".$item->get_id()."]').find('.progress').css('width', progress+'%');
+						}, 50);
+						} else {
+							item".$item->get_id().".pause();
+							item".$item->get_id().".currentTime = 0;
+							clearInterval(item".$item->get_id().".progressupdate);
+							$('[data-item-id=".$item->get_id()."]').find('.progress').css('width', 0);
+						}
+					});
+
+					item".$item->get_id().".onended = function() { 
+						clearInterval(item".$item->get_id().".progressupdate);
+						$('[data-item-id=".$item->get_id()."]').find('.progress').css('width', 0); 
+					}
+
+					item".$item->get_id().".oncanplay = function() { $('[data-item-id=".$item->get_id()."]').removeClass('disabled'); }
+				});
+				</script>
+
+			</div>");
 	} else {
-		echo("<div class=\"well\"></div>");
+		echo("
+			<div class=\"well\"></div>
+		");
 	}
 	echo("</div>");
 	if(((($i+1) % 3) == 0) && ($i < 9)) echo("</div><div class=\"row\">");
