b4e5d08f4276702df6b05d401b9fb4602fcf8780
James Van Hinsbergh <jvanhins@gmail.com>
2015-01-06 21:37:20 +0000

    Moar debugging

--- ajax/import-track.php
+++ ajax/import-track.php
@@ -67,7 +67,8 @@ if(!Session::is_group_user("Importer")) {
 
 	if(!$audio->save()) {
 		unlink($tempfile);
-		die(json_encode(array("error" => "Failed to save audio entry to databases.".var_dump(DigiplayDB::get_last_error()), "debug" => Errors::report("array"))));
+		$error_message = DigiplayDB::get_last_error();
+		die(json_encode(array("error" => "Failed to save audio entry to database.".$error_message["string"], "debug" => Errors::report("array"))));
 	}
 
 	if(!isset($_REQUEST["dir"])) $audio->move_to_music_folder();
