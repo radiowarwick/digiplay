79d9db9ac276be1e7262972400df8522adc6c8ac
James Van Hinsbergh <jvanhins@gmail.com>
2015-01-30 01:03:32 +0000

    Fixed silly error in class

--- includes/SustainerSlot.php
+++ includes/SustainerSlot.php
@@ -3,16 +3,16 @@ class SustainerSlot {
 	protected $id;
 	protected $day;
 	protected $time;
-	protected $playlist_id;
+	protected $playlistid;
 	
 	public function get_id(){ return $this->id; }
 	public function get_day(){ return $this->day; }
 	public function get_time(){ return $this->time; }
-	public function get_playlist_id(){ return $this->playlist_id; }
+	public function get_playlist_id(){ return $this->playlistid; }
 
 	public function set_day($day){ $this->day = $day; }
 	public function set_time($time){ $this->time = $time; }
-	public function set_playlist_id($playlist_id){ $this->playlist_id = $playlist_id; }
+	public function set_playlist_id($playlist_id){ $this->playlist_id = $playlistid; }
 	
 	/* Extended functions */
 	/*public function get_style() { return AudiowallStyles::get_by_id($this->style_id); }
