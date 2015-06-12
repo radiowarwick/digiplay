e6c6e5c9a7deee744216d0153dd0d08ae00ae86e
James Van Hinsbergh <jvanhins@gmail.com>
2015-06-12 12:27:13 +0100

    Add update to JAVO

--- includes/SustainerSlots.php
+++ includes/SustainerSlots.php
@@ -10,7 +10,8 @@ class SustainerSlots{
 	}
 
 	public static function get_all() {
-		return DigiplayDB::select("* FROM sustslots ORDER BY time ASC, case when day = 'm' then 1 when day = 'tu' then 2 when day = 'w' then 3 when day = 'th' then 4 when day = 'f' then 5 when day = 'sa' then 6 when day = 'su' then 7 end", "SustainerSlot");
+		return DigiplayDB::select("* FROM sustslots ORDER BY time ASC, day ASC", "SustainerSlot");
+		//return DigiplayDB::select("* FROM sustslots ORDER BY time ASC, case when day = 'm' then 1 when day = 'tu' then 2 when day = 'w' then 3 when day = 'th' then 4 when day = 'f' then 5 when day = 'sa' then 6 when day = 'su' then 7 end", "SustainerSlot");
 	}
 
 	public static function get_by_playlist($playlist) {
