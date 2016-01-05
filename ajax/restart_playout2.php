b0e0bdb38ac0351e7bdd99d3b518ab4d7509432f
Joe Walton <jwalton@raw.sunion.warwick.ac.uk>
2016-01-05 13:09:02 +0000

    Changed permissions on playout reset

--- ajax/restart_playout2.php
+++ ajax/restart_playout2.php
@@ -1,23 +1,24 @@
 <?php
-//if(Session::is_user()){
+if(Session::is_user()){
+	
 	shell_exec("/root/restart_po2_php");
 
-//	if(Errors::occured()) { 
+	if(Errors::occured()) { 
 
-//		http_response_code(400);
-//		exit(json_encode(array("error" => "Something went wrong. You may have discovered a bug!","detail" => Errors::report("array"))));
-//		Errors::clear();
+		http_response_code(400);
+		exit(json_encode(array("error" => "Something went wrong. You may have discovered a bug!","detail" => Errors::report("array"))));
+		Errors::clear();
 
-//	} else {
+	} else {
 
-//		exit(json_encode(array('response' => 'success', 'id' => 1)));
+		exit(json_encode(array('response' => 'success', 'id' => 1)));
 
-//	}
+	}
 
-//} else {
+} else {
 
-//	http_response_code(403);
-//	exit(json_encode(array('error' => 'Permission denied.')));
+	http_response_code(403);
+	exit(json_encode(array('error' => 'Permission denied.')));
 
-//}
+}
 ?>
