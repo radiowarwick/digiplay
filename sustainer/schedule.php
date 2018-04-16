886b973ceefec0e66d50b2f92e1f5089c9397477
Connor Powell <c.powell924@gmail.com>
2018-04-16 15:01:11 +0100

    Made time work on scheduling modal

--- sustainer/schedule.php
+++ sustainer/schedule.php
@@ -166,7 +166,7 @@ echo(Bootstrap::modal("update-modal", "
 						<select id=\"playlist-id\" name=\"playlist-id\" data-width=\"100%\">
 							".$playlistOptions."
 						</select>
-						<p class=\"help-block\">Select the playlist to be scheduled for <hh>.</p>
+						<p class=\"help-block\">Select the playlist to be scheduled for <span class='schedule-time'></span>.</p>
 					</div>
 				</div>
 			</fieldset>
@@ -176,7 +176,7 @@ echo(Bootstrap::modal("update-modal", "
 					<div class=\"controls\">
 						<select id=\"prerecord-id\" name=\"prerecord-id\" data-width=\"100%\">
 						</select>
-						<p class=\"help-block\">Select the prerecorded content to be played out at <hh>.</p>
+						<p class=\"help-block\">Select the prerecorded content to be played out at <span class='schedule-time'></span>.</p>
 					</div>
 				</div>
 			</fieldset>
@@ -189,6 +189,11 @@ echo(Bootstrap::modal("update-modal", "
 		boxes.dblclick(function(){
 			$('#update-modal').modal('show');
 			$('.update-id').val($(this).attr('id'));
+			splitTimeslot = $(this).attr('id').split('-');
+			days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+			day = (days[splitTimeslot[1]]);
+			time = splitTimeslot[2] + ':00';
+			$('.schedule-time').text(day + ' ' + time);
 			$.ajax({
 				url: '".LINK_ABS."ajax/get-slot-status.php',
 				data: { updateid: $('.update-id').val() },
